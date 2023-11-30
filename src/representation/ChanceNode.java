a66412d5cbdfc70b49d4c89293e3ed5f1b4f0545
Hania Rezk <haniarezk@Hanias-MacBook-Air-2.local>
2023-11-30 22:54:33 +0100

    change ChanceNode

--- src/representation/ChanceNode.java
+++ src/representation/ChanceNode.java
@@ -1,26 +1,27 @@
 package representation;
+import java.util.List;
 import java.util.Random;
 
 public class ChanceNode extends Node{
-	private Node[] possibilites;
+	private List<Node> possibilites;
     private Random random;
-	 public ChanceNode(int id,int depth, String description, Node[] possibilites) {
+	 public ChanceNode(int id,int depth, String description, List<Node> possibilites) {
 	        super(id, depth,description);
 	        this.possibilites=possibilites;
 	        this.random=new Random();
 	    }
 
-	public Node[] getPossibilites() {
+	public  List<Node>getPossibilites() {
 			return possibilites;
 		}
-	public void setPossibilites(Node[] possibilites) {
+	public void setPossibilites( List<Node> possibilites) {
 			this.possibilites = possibilites;
 		}
   //implements random law
 	@Override
 	public Node chooseNext() {
-		int randomIndex = random.nextInt(possibilites.length);
-        return possibilites[randomIndex];
+	    int randomIndex = random.nextInt(possibilites.size());
+	    return possibilites.get(randomIndex);
 	}
 	
 	public ChanceNode cloneNode () {
